6ff32f93406f6b296191b44468dda0148e00606f
Justin Palmer <jpavlav@gmail.com>
2018-10-09 12:49:21 -0500

    Updated Setup.py and uploaded to pypi.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'prometheus'
+NAME = 'query_prom'
 DESCRIPTION = 'Query prometheus for data'
 URL = 'https://github.com/jpavlav/QueryPrometheus'
 EMAIL = 'jpavlav@gmail.com'
@@ -46,6 +46,43 @@ else:
     about['__version__'] = VERSION
 
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
 setup(
     name=NAME,
     version=about['__version__'],
@@ -59,4 +96,18 @@ setup(
     url=URL,
     install_requires=REQUIRED,
     extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
